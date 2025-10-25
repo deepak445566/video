c4e2368f1ac17bab9e31f70b982b5aabced2e2af
deepakpandey121 <deepakpandey55667788@gmail.com>
2025-10-25 20:57:22 +0530

    last

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 
 
@@ -11,8 +12,48 @@ export const protectRoute = async (req,res,next)=>{
       message: "no token provided"
     });
       }
+ const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded token:", decoded); // Debug
+ if (!decoded) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token structure - ownerId not found"
+      });
+    }
+
+   const user = await User.findById(decoded.userId).select("-password");
+
+ if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+    req.user = user;
+    next()
+
+
 
   } catch (error) {
+    console.error("❌ protectUser - Auth middleware error:", error.message);
     
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token"
+      });
+    }
+    
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({
+        success: false,
+        message: "Token expired"
+      });
+    }
+    
+    res.status(500).json({
+      success: false,
+      message: "Server error in authentication"
+    });
   }
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { login, logout, onboard, signup } from "../controllers.js/authControllers.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
 const authRouter = express.Router()
 
 authRouter.post("/signup",signup)
@@ -8,7 +9,7 @@ authRouter.post("/login",login)
 
 authRouter.post("/logout",logout)
 
-authRouter.post("/onboarding",onboard)
+authRouter.post("/onboarding",protectRoute,onboard)
 
 
 export default authRouter
